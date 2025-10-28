e79b8fa8d4c2f207ad2298fc709e17b48483a451
Wilian Martinez <wmartzh@gmail.com>
2025-10-27 18:03:24 -0600

    update

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,7 +1,12 @@
 return {
 	"ibhagwan/fzf-lua",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+		"nvim-mini/mini.nvim",
+	},
 	opts = {
+		glyph_icons = true,
+		file_icons = true,
 		fzf_opts = {
 			["--layout"] = "default",
 		},
@@ -14,6 +19,21 @@ return {
 			cwd_prompt = false,
 			prompt = " 󰅂 ",
 		},
+		buffers = {
+			prompt = "Buffers 󰅂 ",
+			file_icons = true,
+			glyph_icons = true,
+			glyph_map = {
+				["%"] = " ", -- current
+				["#"] = " ", -- alternate
+				["a"] = " ", -- active
+				["h"] = " ", -- hidden
+				["*"] = "●", -- modified
+				["="] = "", -- readonly
+				["-"] = " ", -- unloaded
+				["t"] = " ", -- terminal
+			},
+		},
 		winopts = {
 			preview = {
 				layout = "flex",
@@ -21,19 +41,67 @@ return {
 		},
 	},
 	keys = {
-		{ "<leader>ff", "<cmd>FzfLua files layout=reverse<cr>", desc = "Find Files" },
-		{ "<leader>fF", "<cmd>FzfLua files resume=true<cr>", desc = "Find Files Resume" },
-		{ "gr", "<cmd>FzfLua lsp_references<cr>", desc = "Show LSP references" },
-		{ "<leader>fd", "<cmd>FzfLua lsp_declarations<cr>", desc = "Show LSP declarations" },
-		{ "gt", "<cmd>FzfLua lsp_typedefs<cr>", desc = "Show LSP type definitions" },
-		{ "<leader>fa", "<cmd>FzfLua lsp_document_symbols<cr>", desc = "Show LSP type definitions" },
-		{ "<leader>fg", "<cmd>FzfLua git_status<cr>", desc = "Git Status" },
-		{ "<leader>fb", "<cmd>FzfLua buffers<cr>", desc = "Show current buffers" },
-
-		-- Live Grep
+		{
+			"<leader>ff",
+			function()
+				require("fzf-lua").files({ layout = "reverse" })
+			end,
+			desc = "Find Files",
+		},
+		{
+			"<leader>fF",
+			function()
+				require("fzf-lua").files({ resume = true })
+			end,
+			desc = "Find Files Resume",
+		},
+		{
+			"gr",
+			function()
+				require("fzf-lua").lsp_references()
+			end,
+			desc = "Show LSP references",
+		},
+		{
+			"<leader>fd",
+			function()
+				require("fzf-lua").lsp_declarations()
+			end,
+			desc = "Show LSP declarations",
+		},
+		{
+			"gt",
+			function()
+				require("fzf-lua").lsp_typedefs()
+			end,
+			desc = "Show LSP type definitions",
+		},
+		{
+			"<leader>fa",
+			function()
+				require("fzf-lua").lsp_document_symbols()
+			end,
+			desc = "Show LSP Document Symbols",
+		},
+		{
+			"<leader>fg",
+			function()
+				require("fzf-lua").git_status()
+			end,
+			desc = "Git Status",
+		},
+		{
+			"<leader>fb",
+			function()
+				require("fzf-lua").buffers()
+			end,
+			desc = "Show current buffers",
+		},
 		{
 			"<leader>/",
-			"<cmd>FzfLua live_grep<cr>",
+			function()
+				require("fzf-lua").live_grep()
+			end,
 			desc = "Live Grep",
 		},
 	},
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,94 +1,93 @@
 return {
-  'neovim/nvim-lspconfig',
-  dependencies = {
-    "mason-org/mason-lspconfig.nvim",
-    {
-      "mason-org/mason.nvim",
-      opts = {
-        ui = {
-          icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
-          }
-        }
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"mason-org/mason-lspconfig.nvim",
+		{
+			"mason-org/mason.nvim",
+			opts = {
+				ui = {
+					icons = {
+						package_installed = "✓",
+						package_pending = "➜",
+						package_uninstalled = "✗",
+					},
+				},
+			},
+		},
+	},
+	opts = {
+		automatic_enable = true,
+	},
+	config = function()
+		local capabilitiesOpts = {
+			textDocument = {
+				foldingRange = {
+					dynamicRegistration = false,
+					lineFoldingOnly = true,
+				},
+			},
+		}
+		local capabilities = require("blink.cmp").get_lsp_capabilities(capabilitiesOpts)
 
-      }
-    }
-  },
-  opts = {
-    automatic_enable = true
-  },
-  config = function()
-    local capabilitiesOpts = {
-      textDocument = {
-        foldingRange = {
-          dynamicRegistration = false,
-          lineFoldingOnly = true
-        }
-      }
-    }
-    local capabilities = require('blink.cmp').get_lsp_capabilities(capabilitiesOpts)
+		local on_attach = function(_, bufnr)
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {
+				buffer = bufnr,
+				desc = "Go to definition",
+			})
+			vim.keymap.set("n", "gD", vim.lsp.buf.type_definition, {
+				buffer = bufnr,
+				desc = "Go to type definition",
+			})
+			vim.keymap.set("n", "gD", vim.lsp.buf.type_definition, {
+				buffer = bufnr,
+				desc = "Go to type definition",
+			})
+			vim.keymap.set("n", "gr", function()
+				require("fzf-lua").lsp_references()
+			end, {
+				buffer = bufnr,
+				desc = "Go to references (FZF)",
+			})
+			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {
+				buffer = bufnr,
+				desc = "Go to implementation",
+			})
+			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {
+				buffer = bufnr,
+				desc = "Rename symbol",
+			})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {
+				buffer = bufnr,
+				desc = "See code actions",
+			})
+		end
 
-    local on_attach = function(_, bufnr)
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {
-        buffer = bufnr,
-        desc = "Go to definition",
-      })
-      vim.keymap.set("n", "gD", vim.lsp.buf.type_definition, {
-        buffer = bufnr,
-        desc = "Go to type definition",
-      })
-      vim.keymap.set("n", "gD", vim.lsp.buf.type_definition, {
-        buffer = bufnr,
-        desc = "Go to type definition",
-      })
-      vim.keymap.set("n", "gr", "<cmd>FzfLua lsp_references<cr>", {
-        buffer = bufnr,
-        desc = "Go to references (FZF)",
-      })
-      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {
-        buffer = bufnr,
-        desc = "Go to implementation",
-      })
-      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {
-        buffer = bufnr,
-        desc = "Rename symbol",
-      })
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {
-        buffer = bufnr,
-        desc = "See code actions",
-      })
-    end
+		local default_config = {
+			capabilities = capabilities,
+			on_attach = on_attach,
+		}
 
-    local default_config = {
-      capabilities = capabilities,
-      on_attach = on_attach,
-    }
+		local lua_ls = require("config.lsp_servers.lua_ls")
+		-- local ts_ls = require("config.lsp_servers.ts_ls")
+		-- config lua server
+		vim.lsp.config("lua_ls", vim.tbl_deep_extend("force", default_config, lua_ls))
 
-    local lua_ls = require("config.lsp_servers.lua_ls")
-    -- local ts_ls = require("config.lsp_servers.ts_ls")
-    -- config lua server
-    vim.lsp.config('lua_ls', vim.tbl_deep_extend('force', default_config, lua_ls))
+		vim.lsp.config("deno", {
+			settings = {
+				deno = {
+					enable = true,
+					lint = true,
+				},
+			},
+			root_markers = { { "deno.json", "deno.jsonc" } },
+		})
 
-    vim.lsp.config('deno', {
-      settings = {
-        deno = {
-          enable = true,
-          lint = true,
-        },
-      },
-      root_markers = { { "deno.json", "deno.jsonc" } },
+		vim.lsp.config("gopls", default_config)
 
-    })
-
-    vim.lsp.config('gopls', default_config)
-
-
-    vim.lsp.enable({
-      'lua_ls',
-      'gopls',
-      -- 'denols'
-    })
-  end
+		vim.lsp.enable({
+			"lua_ls",
+			"gopls",
+			-- 'denols'
+		})
+	end,
 }
