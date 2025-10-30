5710d5e16f1c296202f99fe583d093dc3ecbd455
Wilian Martinez <wmartzh@gmail.com>
2025-10-30 10:22:44 -0600

    add new keymaps

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -199,5 +199,40 @@ return {
 			desc = "Git Browse",
 			mode = { "n", "v" },
 		},
+		{
+			"<leader><space>",
+			function()
+				Snacks.picker.smart()
+			end,
+			desc = "Smart Find Files",
+		},
+		{
+			"<leader>,",
+			function()
+				Snacks.picker.buffers()
+			end,
+			desc = "Buffers",
+		},
+		{
+			"<leader>/",
+			function()
+				Snacks.picker.grep()
+			end,
+			desc = "Grep",
+		},
+		{
+			"<leader>:",
+			function()
+				Snacks.picker.command_history()
+			end,
+			desc = "Command History",
+		},
+		{
+			"<leader>n",
+			function()
+				Snacks.picker.notifications()
+			end,
+			desc = "Notification History",
+		},
 	},
 }
